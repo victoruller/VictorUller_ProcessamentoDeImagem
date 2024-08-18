138874c3e393ed4ea62f47277395be20757a857e
victoruller <102265044+victoruller@users.noreply.github.com>
2024-08-18 15:43:44 -0300

    transformei o código em uma função

--- Convolucao.py
+++ Convolucao.py
@@ -8,44 +8,42 @@ cv2.destroyAllWindows()
 
 import numpy as np
 import cv2
-from google.colab.patches import cv2_imshow #X
-
-img_in = cv2.imread('imagemVivaPinhata.webp')
-print(img_in.shape)
-img_out = np.zeros(img_in.shape, dtype=np.uint8)
-
-kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-divisor = 0
-for aux in range(3):
-  for aux2 in range(3):
-    divisor += kernel[aux, aux2]
 
 
-for k in range(img_out.shape[2]):
-  for i in range(img_out.shape[1]):
-    for j in range(img_out.shape[0]):
-      auxx = 0
-      for I in np.array([-1, 0, 1]):
-        for J in np.array([-1, 0, 1]):
-          if(i+I < 0 or i+I >= img_in.shape[1] or j+J < 0 or j+J >= img_in.shape[0]):
-            if(j+J >= 0 and j+J < img_in.shape[0]):
-              auxx += (img_in[j+J, i, k] * kernel[I+1, J+1])
-              continue
-            elif(i+I >= 0 and i+I < img_in.shape[1]):
-              auxx += (img_in[j, i+I, k] * kernel[I+1, J+1])
-              continue
+def fazConvolucao(imageLink, kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]))
+  img_in = cv2.imread(imageLink)
+  print(img_in.shape)
+  img_out = np.zeros(img_in.shape, dtype=np.uint8)
+  
+  divisor = 0
+  for aux in range(3):
+    for aux2 in range(3):
+      divisor += kernel[aux, aux2]
+  
+  
+  for k in range(img_out.shape[2]):
+    for i in range(img_out.shape[1]):
+      for j in range(img_out.shape[0]):
+        auxx = 0
+        for I in np.array([-1, 0, 1]):
+          for J in np.array([-1, 0, 1]):
+            if(i+I < 0 or i+I >= img_in.shape[1] or j+J < 0 or j+J >= img_in.shape[0]):
+              if(j+J >= 0 and j+J < img_in.shape[0]):
+                auxx += (img_in[j+J, i, k] * kernel[I+1, J+1])
+                continue
+              elif(i+I >= 0 and i+I < img_in.shape[1]):
+                auxx += (img_in[j, i+I, k] * kernel[I+1, J+1])
+                continue
+              else:
+                auxx += (img_in[j, i, k] * kernel[I+1, J+1])
+                continue
             else:
-              auxx += (img_in[j, i, k] * kernel[I+1, J+1])
+              auxx += (img_in[j+J, i+I, k] * kernel[I+1, J+1])
               continue
-          else:
-            auxx += (img_in[j+J, i+I, k] * kernel[I+1, J+1])
-            continue
-
-      #print(img_out[i, j, k], end=' -> ')
-      img_out[j, i, k] =  int((auxx / divisor))
-      #print(img_out[i, j, k])
-
-cv2_imshow(img_out)
+              
+        img_out[j, i, k] =  int((auxx / divisor))
+  
+ return img_out
         
           
 
